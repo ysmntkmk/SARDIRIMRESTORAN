b09abdbcc966400a5411e9fa9fd260a3cb0d0cda
Yasemin tokmak <yasemintokmak10@hotmail.com>
2024-03-08 18:20:10 +0300

    .

--- NTierSardırımRes.MVC/Models/ViewModels/CustomerViewModel/CustomerCreateVM.cs
+++ NTierSardırımRes.MVC/Models/ViewModels/CustomerViewModel/CustomerCreateVM.cs
@@ -1,14 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using NTierSardırımRes.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace NTierSardırımRes.MVC.Models.ViewModels.CustomerVİewModel
 {
     public class CustomerCreateVM
+
     {
+        public CustomerCreateVM() 
+        {
+           DataStatus status = DataStatus.Inserted;
+        }
         [Required(ErrorMessage = " adı boş geçilemez!")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = " Soyadı boş geçilemez!")]
         public string LastName { get; set; }
-       
+        public string? Adress { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string Email { get; set; }
+
+
     }
 }
