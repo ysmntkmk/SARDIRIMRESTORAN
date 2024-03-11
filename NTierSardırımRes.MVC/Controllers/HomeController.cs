acec4939b5d65edd434f0849457dd66b972b6e61
Yasemin tokmak <yasemintokmak10@hotmail.com>
2024-03-11 22:43:49 +0300

    ASkıma özel mesaj

--- NTierSardırımRes.MVC/Controllers/HomeController.cs
+++ NTierSardırımRes.MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace NTierSardırımRes.MVC.Controllers
 {
+    //Askım seni cok seviyorum
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
